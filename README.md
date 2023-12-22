63134f8dc00ec41a9b35a05122a9b9fab02412c9
Éléa Dufresne <elea.dufresne@mail.mcgill.ca>
2023-12-22 17:05:21 -0500

    windowed streaming monitoring dir; added README and examples

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# fruit-stream-processor
- windowed stream processing for monitoring fruit trees
+# Overview
+ This is a windowed stream processing for monitoring fruit tree images from a directory.
+
+# Requirements
+You will need to clone this repository into the home directory of your UNIX-like systems, including Linux, Mac OS X, 
+and [Cygwin](https://cygwin.com/install.html) (for Windows) and ensure that Apache Flink is installed.
+```shell
+git clone https://github.com/eleadufresne/fruit-stream-processor.git
+```
+Flink is compatible with all To run Flink, it's essential to have [Java 11](https://www.oracle.com/java/technologies/javase/jdk11-archive-downloads.html) installed 
+on your system. You can verify the installed version by executing `$ java -version` in your command prompt. Follow this 
+[tutorial](https://nightlies.apache.org/flink/flink-docs-stable/docs/try-flink/local_installation/#first-steps) to get 
+started with Flink. 🙃
+
+# Configuration
+Please keep in mind the following steps when setting up Cygwin. If you're using a different environment, you may need 
+to make some additional configurations. 
+
+To begin with, ensure that you've installed the latest Flink distribution, mintty and netcat. Then, navigate to the 
+Cygwin folder and locate the ```.bash_profile``` file in the home directory. Add the two lines provided below to the 
+end of the file and save it:
+
+```shell
+export SHELLOPTS
+set -o igncr
+```
+These commands ensure proper handling of command line arguments in all child processes and normal behavior of the 
+carriage return when executing a command in Cygwin.
+
+Next, download the latest Flink distribution and open the flink-conf.yaml file. Add the following line to the end of the file:
+
+```shell
+taskmanager.resource-id: stream-processing-logs
+```
+Flink will create a temporary folder under Cygwin with the given name to maintain its state logs as it sets up a cluster.
+
+# Running the Application
+From the root directory of your UNIX-like system, execute the following commands.
+
+```shell
+# start a cluster
+cd flink* && ./bin/start-cluster.sh
+# generate a JAR file for the Flink job
+cd ../fruit* && mvn clean package && cd ../flink* 
+# start monitoring the files in the "data" directory
+./bin/flink run ../fruit*/tar*/fruit*.jar --path data/
+```
+Some example files are provided in the util folder for monitoring. You now visualize the job on the Flink dashboard 
+on [localhost:8081](http://localhost:8081).
--- src/main/java/com/oranges/streamprocessing/FruitStreaming.java
+++ src/main/java/com/oranges/streamprocessing/FruitStreaming.java
@@ -1,63 +1,66 @@
+/**
+ * Flink-based data processing app that reads text files from a directory, filters and counts
+ * different types of oranges, and prints the result. To run the app, specify the directory path
+ * as a command-line argument. For example:
+ *
+ * ./bin/flink run /path/to/fruit-streaming-1.jar --path data/
+ *
+ * useful documentation :
+ * https://nightlies.apache.org/flink/flink-docs-release-1.18/docs/dev/datastream/overview/#anatomy-of-a-flink-program
+ *
+ */
+
 package com.oranges.streamprocessing;
 
+import java.time.Duration;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-
-// medium
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.util.Collector;
 
-/* Build this artifact by running  ``mvn clean package`` in the root directory.
-*
-* TERMINAL #1
-*   nc -l 9999
-*
-* TERMINAL #2
-*  cd flink* && .bin/start-cluster
-*  cd ../fruit* && mvn clean package && cd ../flink*
-*  ./bin/flink run ../fruit* /tar* /fruit*.jar
-*/
+import org.apache.flink.connector.file.src.FileSource;
+
+
+/** @author Éléa Dufresne */
 public class FruitStreaming {
 
-    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
-        @Override
-        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
-            for (String word: sentence.split(" ")) {
-                out.collect(new Tuple2<String, Integer>(word, 1));
-            }
-        }
-    }
-
+    /** MapFunction takes a string and maps it to a tuple of string and integer */
     public static final class Tokenizer implements MapFunction<String, Tuple2<String, Integer>> {
+        /** @param s input string to be mapped
+         *  @return a Tuple2 containing the input string paired with the number 1 */
         @Override
-        public Tuple2<String, Integer> map(String s) throws Exception {
-            return new Tuple2<String, Integer>(s, 1);
-        }
+        public Tuple2<String, Integer> map(String s) { return new Tuple2<>(s, 1); }
     }
 
+    /** main method that executes the Flink app
+     * @param args command-line arguments */
     public static void main(String[] args) throws Exception {
-        /* steps from: https://nightlies.apache.org/flink/flink-docs-release-1.18/docs/dev/datastream/overview/#anatomy-of-a-flink-program
-         * 1. Obtain an execution environment (main entry point to building Flink applications) */
+        /* 1. Obtain an execution environment (main entry point to building Flink applications) */
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         /* 2. Load/create the initial data */
         final ParameterTool params = ParameterTool.fromArgs(args); // input
-        env.getConfig().setGlobalJobParameters(params);  // make the input available in the Flink UI
-        DataStream<String> data_stream = env.socketTextStream("localhost", 9999); // get input stream from port 9999
 
-        DataStream<Tuple2<String, Integer>> orange_count = data_stream
-            .filter((FilterFunction<String>) value -> value.endsWith("orange")) // keep oranges
-            .map(new Tokenizer()) // split up the lines in pairs (2-tuples) containing: tuple2 {(rotten,1)...}
-            .keyBy(value -> value.f0) // group by the tuple field "0" (orange feature)
-            .window(TumblingProcessingTimeWindows.of(Time.seconds(10))) // every 5 seconds
-            .sum(1); // sum up tuple field "1" (the number of such oranges)
+        env.getConfig().setGlobalJobParameters(params);  // make the input available in the Flink UI
+
+        String path_to_dir = params.get("path", "data/"); // directory to monitor
+
+        FileSource<String> source_dir = FileSource
+            .forRecordStreamFormat(new TextLineInputFormat(), new Path(path_to_dir))
+            .monitorContinuously(Duration.ofSeconds(10)) // monitor every 10s
+            .build();
+
+        DataStream<String> data_stream = env.fromSource(
+            source_dir,
+            WatermarkStrategy.noWatermarks(),
+            "source directory");
 
         /* https://github.com/SparkWorksnet/demoFlink/blob/master/src/main/java/net/sparkworks/stream/StreamProcessor.java
         // Define the window and apply the reduce transformation
@@ -69,6 +72,13 @@ public class FruitStreaming {
         // TODO take input from a directory of files
 
         /* 3. Specify transformations on this data */
+
+        DataStream<Tuple2<String, Integer>> orange_count = data_stream
+            .filter((FilterFunction<String>) value -> value.endsWith("orange")) // keep oranges
+            .map(new Tokenizer()) // split up the lines in pairs (2-tuples) containing: tuple2 {(rotten,1)...}
+            .keyBy(value -> value.f0) // group by the tuple field "0" (orange feature)
+            .window(TumblingProcessingTimeWindows.of(Time.seconds(5))) // every 5 seconds
+            .sum(1); // sum up tuple field "1" (the number of such oranges)
 
         // TODO replace words by images
 
